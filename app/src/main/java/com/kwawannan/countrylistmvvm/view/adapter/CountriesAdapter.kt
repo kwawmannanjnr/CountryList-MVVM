bcabcc982727f2395928ff68efe072258eaa72ca
Kwaw Annan <kwawmannanjnr@gmail.com>
2024-03-01 14:25:14 -0800

    Implementing the CountriesAdapter

--- app/src/main/java/com/kwawannan/countrylistmvvm/view/adapter/CountriesAdapter.kt
+++ app/src/main/java/com/kwawannan/countrylistmvvm/view/adapter/CountriesAdapter.kt
@@ -3,6 +3,7 @@ package com.kwawannan.countrylistmvvm.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kwawannan.countrylistmvvm.Network.model.Countries
@@ -11,8 +12,8 @@ import com.kwawannan.countrylistmvvm.R
 
 class CountriesAdapter(private val countries : Countries): RecyclerView.Adapter<CountriesAdapter.CountryViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-        return CountryViewHolder(null)
-    }
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.country_item, parent, false)
+        return CountryViewHolder(itemView)    }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
         val country = countries[position]
@@ -22,7 +23,18 @@ class CountriesAdapter(private val countries : Countries): RecyclerView.Adapter<
     override fun getItemCount(): Int = countries.size
 
     class CountryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val flagImageView: ImageView = itemView.findViewById(R.id.flagImageView)
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        private val capitalTextView: TextView = itemView.findViewById(R.id.capitalTextView)
+
         fun bind(country: Country) {
+             // I used Glide to load the flag image from URL
+
+//            Glide.with(itemView.context)
+//                .load(country.flag)
+//                .into(flagImageView) nameTextView.text = country.name
+            capitalTextView.text = country.capital
         }
     }
 }
