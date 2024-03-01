3e391ce23d47e9778331cfb28d76805c9accfe2e
Kwaw Annan <kwawmannanjnr@gmail.com>
2024-03-01 12:59:37 -0800

    created Countries ViewModel signature.

---
+++ app/src/main/java/com/kwawannan/countrylistmvvm/Network/model/viewmodel/CountriesViewModel.kt
@@ -0,0 +1,6 @@
+package com.kwawannan.countrylistmvvm.Network.model.viewmodel
+
+import com.kwawannan.countrylistmvvm.Network.model.repository.CountriesRepository
+
+class CountriesViewModel(private val repository: CountriesRepository) {
+}
